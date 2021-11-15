29f855fc27a18319431ec9af26da1149e441cfa3
Bruno Cordeiro Ribeiro <brunocordeiroribeiro130904@gmail.com>
2021-11-15 10:55:02 -0300

    Update Eletroeletronica-Escolar

--- Trabalho-Escolar/EletronicaDigital FlipFlop/parte1.py
+++ Trabalho-Escolar/EletronicaDigital FlipFlop/parte1.py
@@ -2,45 +2,43 @@ import time
 
 class caixa_água():
 
-    def __init__(self):
+    def __init__(self, tamanho_caixa):
 
-        self.estado_de_clock = False
+        self.tamanho_caixa = tamanho_caixa
+        self.pulso = 0 #clock zero, pois o circuito está desligado
         self.circuito = False 
         self.litros_água = 0
-        self.tamanho_caixa = 50
-        self.pulso = 0
-        self.contador_de_litros = 0
 
-    def ligando(self):
-
-        while self.estado_de_clock == True:
-            print('Clock desligado...')
-            self.pulso = str(input('Deseja ligar o circuito? [S/N]: ')).upper()
-            if self.pulso == 'S':
-                self.pulso = 1
-                self.estado_de_clock = True
-                print('Clock Ligado...')
+    def iniciando(self, ligar):
+        
+        if ligar == 0:
+            print('Circuito desligado...')
+        if ligar == 1:
+                self.pulso = 1 #se o pulso for igual a 1 liga o circuito
+                print('Dando o pulso ao circuito...')
                 print('Circuito não danificado, iniciando o circuito...')
                 time.sleep(1)
-                self.circuito = True
-                print('Clock tirado...')
-                self.estado_de_clock = False
-                break #acho que não precisava disso, mas tá bugado
-
+                self.circuito = True #ligando o circuito
+                self.pulso = 0 # Tirando o pulso
+                print('Pulso tirado! Não necessita pressionar o botão. ')
+        
     def processo(self):
 
-        self.circuito = True #Tenho que declarar isso
         while self.circuito == True:
-            self.contador_de_litros += 1
             self.litros_água += 1
-            print(f'Passou um minuto, {self.contador_de_litros} litros foi colocado. ')
+            print(f'Passou um minuto, {self.litros_água} litros foi colocado. ')
             time.sleep(1)
             if self.litros_água == (self.tamanho_caixa - 2):
                 print('Caixa quase lotada! Desligando o circuito...')
                 time.sleep(1)
-                self.circuito == False
                 print('Desligando...')
-                break #acho que não precisava disso, mas tá bugado
-    
-caixa_água().ligando()
-caixa_água().processo()
+                time.sleep(1)
+                self.circuito = False
+
+caixa = caixa_água(int(input('Qual o tamanho da caixa de água? ')))
+deseja_ligar = str(input('Deseja ligar o circuito? [S/N]: ')).upper()    
+if deseja_ligar == 'N':
+    caixa.iniciando(0)
+elif deseja_ligar == 'S':
+    caixa.iniciando(1)
+    caixa.processo()
